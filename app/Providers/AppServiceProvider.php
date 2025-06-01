0b2dd0d58068eacfc09a2474d4ac6159759b5d04
biguard1 <bogdasha05@gmail.com>
2025-06-02 02:32:56 +0300

    fix: try with proxies

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,12 +19,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (app()->isProduction() && str(config("app.url"))->startsWith('https://')) {
-            URL::forceScheme('https');
-            $this->app['request']->server->set('HTTPS', 'on');
-        }
-
-        URL::useOrigin(config('app.url'));
         //
     }
 }
